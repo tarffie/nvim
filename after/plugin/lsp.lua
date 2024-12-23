2c85c43f605ee05ab36b6e9900d18f8861f4de15
Yoru <enbrakaffe@gmail.com>
2024-12-23 03:31:07 -0300

    feat: ocaml setup, new format plugin, new bindings

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,29 +1,40 @@
 local lsp = require('lsp-zero')
 lsp.on_attach(function(client, bufnr)
-	lsp.default_keymaps({buffer = bufnr})
+  lsp.default_keymaps({ buffer = bufnr })
 end)
 lsp.preset('recommended')
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-	ensure_installed = {'tsserver', 'rust_analyzer', 'lua_ls', 'clangd', 'eslint'},
-	handlers = {
-		function(server_name)
-			require('lspconfig')[server_name].setup({})
-		end,
-	}
+  ensure_installed = { 'rust_analyzer', 'lua_ls', 'clangd', 'eslint', 'jdtls', 'phpactor', 'ocamllsp' },
+  handlers = {
+    function(server_name)
+      require('lspconfig')[server_name].setup({
+        intelephense = function()
+          require('lspconfig').intelephense.setup({
+            root_dir = function()
+              return vim.loop.cwd()
+            end,
+          })
+          require('lspconfig').ocamllsp.setup({
+            on_attach = on_attach,
+          })
+        end,
+      })
+    end,
+  }
 })
 
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-	['<C-y>'] = cmp.mapping.confirm({ select = true }),
-	['<C-Space>'] = cmp.mapping.complete(),
+  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ['<C-Space>'] = cmp.mapping.complete(),
 })
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+  local opts = { buffer = bufnr, remap = false }
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
@@ -37,23 +48,25 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 lsp.configure('lua-language-server', {
-	settings = {
-		Lua = {
-			diagnostics = {
-				globals = { 'vim' }
-			}
-		}
-	}
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' }
+      }
+    }
+  }
 })
 
+
+
 lsp.set_preferences({
-    suggest_lsp_servers = false,
-    sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
-    }
+  suggest_lsp_servers = false,
+  sign_icons = {
+    error = 'E',
+    warn = 'W',
+    hint = 'H',
+    info = 'I'
+  }
 })
 vim.diagnostic.config({
   virtual_text = true,
@@ -61,7 +74,7 @@ vim.diagnostic.config({
   float = {
     style = 'minimal',
     border = 'square',
-    source = 'always',
+    source = true,
     header = '',
     prefix = '',
   },
---
+++ lua/tarffie/init.lua
@@ -0,0 +1,4 @@
+vim.opt.termguicolors = true
+vim.cmd 'colorscheme moonfly'
+
+vim.opt.runtimepath:append("<SHARE_DIR>/merlin/vim")
