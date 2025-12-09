ec47bdf726ed16ccb024f68992b863622e04880f
Emilia Alves <emiliaalves.tarffie@gmail.com>
2025-12-09 03:19:01 -0300

    Fixed init file and removed colors file

--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
 require("tarffie.remap")
 require("tarffie.lazy")
 require("tarffie.set")
+
+vim.opt.termguicolors = true
+vim.cmd 'colorscheme moonfly'
+vim.g.moonflyCursorColor = true
+vim.g.moonflyTransparent = true
+-- vim.cmd 'colorscheme vim-dogrun'
+
+vim.opt.runtimepath:append("<SHARE_DIR>/merlin/vim")
--- lua/tarffie/init.lua
+++
@@ -1,5 +0,0 @@
-vim.opt.termguicolors = true
--- vim.cmd 'colorscheme moonfly'
-vim.cmd 'colorscheme vim-dogrun'
-
-vim.opt.runtimepath:append("<SHARE_DIR>/merlin/vim")
