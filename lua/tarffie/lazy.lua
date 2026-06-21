89f4039bcc3f658521c3fe09686ef8620f8a4eb5
Emilia Alves <emiliaalves.tarffie@gmail.com>
2026-06-21 20:43:49 -0300

    Update: Finally removing lspzero from my deps

--- lua/tarffie/lazy.lua
+++ lua/tarffie/lazy.lua
@@ -1,5 +1,6 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
@@ -7,23 +8,33 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
       { out,                            "WarningMsg" },
-      { "\nPress any key to exit..." },
+      { "\nPress any key to exit..." }
     }, true, {})
     vim.fn.getchar()
     os.exit(1)
   end
 end
+
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   {
-    "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000,
+    "bluz71/vim-moonfly-colors",
+    name = "moonfly",
+    lazy = false,
+    priority = 1000
   },
   {
-    "sainnhe/gruvbox-material", name = "gruvbox-material", lazy = false, priority = 1000,
+    "sainnhe/gruvbox-material",
+    name = "gruvbox-material",
+    lazy = false,
+    priority = 1000
   },
   {
-    "wadackel/vim-dogrun", name = "vim-dogrun", lazy = false, priority = 1000,
+    "wadackel/vim-dogrun",
+    name = "vim-dogrun",
+    lazy = false,
+    priority = 1000
   },
   {
     'nvim-telescope/telescope.nvim',
@@ -31,7 +42,8 @@ require("lazy").setup({
     dependencies = { 'nvim-lua/plenary.nvim' }
   },
   {
-    "nvim-treesitter/nvim-treesitter", build = ":TSUpdate"
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate"
   },
   {
     "ThePrimeagen/harpoon",
@@ -40,21 +52,21 @@ require("lazy").setup({
   },
   {
     "mbbill/undotree",
-    branch = "master",
+    branch = "master"
   },
   {
-    "tpope/vim-fugitive", branch = "master",
+    "tpope/vim-fugitive",
+    branch = "master"
   },
   { 'folke/tokyonight.nvim' },
-  { 'VonHeikemen/lsp-zero.nvim', branch = 'v3.x' },
   { 'williamboman/mason.nvim' },
   {
     "mason-org/mason-lspconfig.nvim",
     opts = {},
     dependencies = {
       { "mason-org/mason.nvim", opts = {} },
-      { "neovim/nvim-lspconfig" },
-    },
+      { "neovim/nvim-lspconfig" }
+    }
   },
   { 'hrsh7th/cmp-nvim-lsp' },
   { 'hrsh7th/nvim-cmp' },
@@ -70,8 +82,8 @@ require("lazy").setup({
     event = { "BufReadPre", "BufNewFile" },
     dependencies = {
       "williamboman/mason.nvim",
-      "nvimtools/none-ls.nvim",
-    },
+      "nvimtools/none-ls.nvim"
+    }
   },
   {
     "nvim-telescope/telescope-file-browser.nvim",
@@ -83,6 +95,6 @@ require("lazy").setup({
     config = function()
       require('java').setup()
       vim.lsp.enable('jdtls')
-    end,
-  },
+    end
+  }
 })
